aa35a9d360c2877e2550a43c1fca34213b76a048
Hybrid Cluster <hybrid@dev1.hybrid-cluster.demo>
2011-04-16 13:36:04 +0000

    Fix exception

--- txmysql/error.py
+++ txmysql/error.py
@@ -1,4 +1,4 @@
 class MySQLError(Exception):
-    def __init__(self, message, errno=None, sqlstate=None):
-        super(MySQLError, self).__init__(message, errno, sqlstate)
-        self.msg, self.errno, self.sqlstate = message, errno, sqlstate
+    def __init__(self, message, errno=None, sqlstate=None, query=None):
+        super(MySQLError, self).__init__(message, errno, sqlstate, query)
+        self.msg, self.errno, self.sqlstate, self.query = message, errno, sqlstate, query
